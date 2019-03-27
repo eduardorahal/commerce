cb7a97739575455aa1f7733920c8d835d449b22b
Eduardo Rahal <eduardorahal@gmail.com>
2019-03-27 00:51:25 -0300

    soap-working

--- controllers/product.js
+++ controllers/product.js
@@ -3,33 +3,67 @@ const Category = require('../models/category');
 const mongoose = require('mongoose');
 var when = require('when');
 var soap = require('soap');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 
+
+//Get Last Date Exchange Rate
+    
+    var url = 'http://infovalutar.ro/curs.asmx?wsdl';
+    
+    var date = soap.createClientAsync(url).then((client) => {
+        return client.lastdateinsertedAsync();
+    }).then((result) => {
+        var result1 = new Date(result[0].lastdateinsertedResult);
+        var result2 = result1.getFullYear() + "-" + (result1.getMonth()<9 ? "0"+(result1.getMonth()+1) : (result1.getMonth()+1)) + "-" + result1.getDate();
+        return result2;
+    });
+
+    
 
 // Select One Product
 exports.getProduct = (req, res, next) => {
-    var date = yyyymmdd();
-    var url = 'http://infovalutar.ro/curs.asmx?wsdl';
-    var args = {dt: date};
-    let p = soap.createClientAsync(url).then((client) => {
-            return client.getallAsync(args);
-          }).then((result) => {
-            return result;
-          });
-    when.all([
-        Category.find({}, {'id':1, 'name':1 }).exec(),
-        Category.find({"id":req.params.cat},{"id":1,"name":1}).exec(),
-        Category.aggregate([
-            { $match: { id: req.params.cat } } ,
-            { $unwind: "$categories" },
-            { $unwind: "$categories.categories" },
-            { $match: { "categories.categories.id":req.params.end} }
-            ]).exec(),
-        Product.find({"id":req.params.id}).exec(),
-        p
+    
+    p = when.all([
+        url,
+        date
     ])
-    .then(docs => {
-        res.render('../views/pages/product', {main:docs[0], cat:docs[1], sub:docs[2], id:docs[3], p:docs[4][0].getallResult.diffgram.DocumentElement.Currency});
-    })
+    .then(data => {
+        url = data[0];
+        date = data[1];
+        args = {dt: date};
+        
+        when.all([
+            soap.createClientAsync(url).then((client) => {
+                return client.getallAsync(args);
+            }).then((result) => {
+                return result;
+            })
+        ])
+        .then(result => {
+            p = result[0][0].getallResult.diffgram.DocumentElement.Currency;
+            
+
+            when.all([
+                Category.find({}, {'id':1, 'name':1 }).exec(),
+                Category.find({"id":req.params.cat},{"id":1,"name":1}).exec(),
+                Category.aggregate([
+                    { $match: { id: req.params.cat } } ,
+                    { $unwind: "$categories" },
+                    { $unwind: "$categories.categories" },
+                    { $match: { "categories.categories.id":req.params.end} }
+                    ]).exec(),
+                Product.find({"id":req.params.id}).exec(),
+                p
+            ])
+            .then(docs => {
+                res.render('../views/pages/product', {main:docs[0], cat:docs[1], sub:docs[2], id:docs[3], p:docs[4]});
+            })
+
+
+        })
+    });
+    
 }
 
 // Select Products of a Category
@@ -47,32 +81,5 @@ exports.getProducts = (req, res, next) => {
     ])
     .then(docs => {
         res.render('../views/pages/products', {main:docs[0], cat:docs[1], sub:docs[2], end:docs[3]});
-    })
-}
-
-
-// Function to Get Date formatted
-
-function yyyymmdd() {
-    var now = new Date();
-    var y = now.getFullYear();
-    var m = now.getMonth() + 1;
-    var d = now.getDate();
-    var d = d - 1;
-    var mm = m < 10 ? '0' + m : m;
-    var dd = d < 10 ? '0' + d : d;
-    return '' + y + "-" + mm + "-" + dd;
-}
-
-
-// Function to get data from SOAP
-function findSoap (req, res, next) {
-    date = yyyymmdd();
-    url = 'http://infovalutar.ro/curs.asmx?wsdl';
-    args = {dt: date};
-    soap.createClient(url, function(err, client) {
-        client.getall(args, function(err, result) {
-            return result
-        })
     })
 }
